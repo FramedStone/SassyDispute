676c2a2771828d6805bfb8ec2aa91c26c3abf47f
vanoss222 <leeweixuan39@gmail.com>
2024-10-04 16:28:39 +0800

    docs: setup hardhat config to deploy onto Scroll Sepolia Testnet

--- hardhat-SD/hardhat.config.ts
+++ hardhat-SD/hardhat.config.ts
@@ -3,6 +3,32 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io" || "",
+      accounts: [
+        "6353790dfd40e2b1a07322384a3f78aa1bf1de33f46bf4ef9033564eeeb61fdb",
+      ],
+    },
+  },
+  etherscan: {
+    apiKey: {
+      scrollSepolia: "M4UK28RICB5BEUU1HKDD2SX1NMBNYYDTWN",
+    },
+    customChains: [
+      {
+        network: "scrollSepolia",
+        chainId: 534351,
+        urls: {
+          apiURL: "https://api-sepolia.scrollscan.com/api",
+          browserURL: "https://sepolia.scrollscan.com/",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    enabled: false,
+  },
 };
 
 export default config;
