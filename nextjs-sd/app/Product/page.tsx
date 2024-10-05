0dcc332ab41395ebff445e3300c3ee4b98c5e6b2
IanGanYTL <ian.gan@ytlcement.com.my>
2024-10-05 12:19:59 +0800

    added the product details with filter but still hardcoded data

--- nextjs-sd/app/Product/page.tsx
+++ nextjs-sd/app/Product/page.tsx
@@ -1,15 +1,9 @@
-import Filter from "@/components/pages/Product/filter";
-import Content from "@/components/pages/Product/Content";
-import TopHeader from "@/components/pages/TopHeader";
+import DisputeCase from "@/components/pages/Product/DisputeCase";
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen bg-gray-900 text-white">
-      <TopHeader />
-      <div className="flex flex-1">
-        <Filter />
-        <Content />
-      </div>
-    </div>
+    <>
+      <DisputeCase />
+    </>
   );
 }
