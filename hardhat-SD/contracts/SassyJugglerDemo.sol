246ef62a4cf9e5c74fbc9c0bcdd1d12c350893c0
vanoss222 <leeweixuan39@gmail.com>
2024-10-05 23:24:46 +0800

    feat: added SassyJugglerDemo.sol for demo purpose only.

---
+++ hardhat-SD/contracts/SassyJugglerDemo.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: GPL-3.0-only
+pragma solidity ^0.8.0;
+
+/* 
+    Sassy Juggler Demo
+    - interface schema for SassyBridge
+    - retrieve dispute cases's ipfsHash from SassyBridge
+    - reward users SassyToken for each participation
+
+*/
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+// SassyToken.sol
+interface ISassyToken {
+    function permit_token(
+        address spender,
+        uint256 value,
+        uint256 deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
+
+contract SassyJugglerDemo is AccessControl {
+    /*
+        SassyJugglerDemo roles setup
+    */
+    event RoleGranted(bytes32 indexed role, address indexed account);
+    event RoleRevoked(bytes32 indexed role, address indexed account);
+
+    bytes32 private constant ADMIN = keccak256(abi.encodePacked("ADMIN"));
+
+    /*
+        SassyJugglerDemo Main Body
+    */
+
+    event disputeCasesAdded(uint256[] caseID, string[] ipfsHash, address indexed submmiter);
+    event disputeCaseInteracted(uint256 caseID, address indexed user);
+    event userRewarded(address indexed user, uint256 rewardAmount);
+
+    address private owner;
+    modifier onlyOwner{
+        require(msg.sender == owner, "not authorized");
+        _;
+    }
+
+    struct disputeCase {
+        string ipfsHash;
+        address submitter;
+        uint256 timestamp;
+    }
+    disputeCase[] public _disputeCases;
+
+    ISassyToken public SassyToken;
+    uint256 public rewardAmount;
+
+    // check if a user has interacted with a case
+    mapping(uint256 => mapping(address => bool)) public hasJuggled;
+
+    constructor(address _owner) {
+        owner = _owner;
+        _setRoleAdmin(DEFAULT_ADMIN_ROLE, ADMIN);
+        _grantRole(ADMIN, owner);
+    }
+
+    // point to SassyToken's smart contract
+    function setSassyToken(address _SassyToken, uint256 _rewardAmount) public onlyOwner {
+        SassyToken = ISassyToken(_SassyToken);
+        rewardAmount = _rewardAmount*10**18;
+    }
+
+    function updateRewardAmount(uint256 _rewardAmount) public onlyRole(ADMIN) {
+        rewardAmount = _rewardAmount*10**18;
+    }
+
+    // load and document dispute cases retrieved from SassyBridge
+    function addDisputeCases(string[] memory _disputeCasesDemo) external onlyRole(ADMIN) {
+        uint256[] memory caseIDs = new uint256[](_disputeCasesDemo.length);
+
+        for (uint256 i = 0; i < _disputeCasesDemo.length; i++) {
+            _disputeCases.push(disputeCase({
+                ipfsHash: _disputeCasesDemo[i],
+                submitter: msg.sender,
+                timestamp: block.timestamp
+            }));
+
+            uint256 caseID = _disputeCases.length - 1;
+            caseIDs[i] = caseID;
+        }
+        emit disputeCasesAdded(caseIDs, _disputeCasesDemo, msg.sender);
+    }
+
+    // interact with dispute case
+    function interactDisputeCase(uint256 _caseID, uint256 deadline, uint8 v, bytes32 r, bytes32 s) public {
+        require(_caseID < _disputeCases.length, "Case does not exist");
+        require(!hasJuggled[_caseID][msg.sender], "User has already started a conversation with this case");
+        
+        hasJuggled[_caseID][msg.sender] = true;
+        emit disputeCaseInteracted(_caseID, msg.sender);
+
+        // reward user with SassyToken
+        SassyToken.permit_token(msg.sender, rewardAmount, deadline, v, r, s);
+        emit userRewarded(msg.sender, rewardAmount);
+    }
+}
