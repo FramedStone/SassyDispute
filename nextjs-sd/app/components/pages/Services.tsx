919543ebeacb4fd563d0c720bc71010693be2c2b
IanGanYTL <ian.gan@ytlcement.com.my>
2024-10-06 08:57:41 +0800

    fixing some link to specific route

--- nextjs-sd/app/components/pages/Services.tsx
+++ nextjs-sd/app/components/pages/Services.tsx
@@ -14,29 +14,21 @@ export default function Services() {
           <div className="grid grid-cols-3 gap-4">
             <div className="col-span-2 grid grid-cols-2 gap-4">
               <div className="bg-[#2F2F32] p-8 min-h-[150px] rounded-lg transition-transform duration-200 hover:scale-105">
-                {" "}
-                {/* Added transition and hover scale */}
                 <p className="text-white text-lg sm:text-3xl">
                   Reward first commentors with SassyTokens
                 </p>
               </div>
               <div className="bg-[#F6FF95] p-8 min-h-[150px] rounded-lg transition-transform duration-200 hover:scale-105">
-                {" "}
-                {/* Added transition and hover scale */}
                 <p className="text-gray-900 text-lg sm:text-3xl">
                   Improve web3 e-commerce experience
                 </p>
               </div>
               <div className="bg-[#F6FF95] col-span-2 p-8 min-h-[150px] rounded-lg transition-transform duration-200 hover:scale-105">
-                {" "}
-                {/* Added transition and hover scale */}
                 <p className="text-gray-900 text-lg sm:text-3xl">
                   Built on top of scroll Layer 2 blockchain solutions
                 </p>
               </div>
               <div className="bg-[#2F2F32] col-span-2 p-8 min-h-[150px] rounded-lg transition-transform duration-200 hover:scale-105">
-                {" "}
-                {/* Added transition and hover scale */}
                 <p className="text-white text-lg sm:text-3xl">
                   Reward first commentors with SassyTokens
                 </p>
@@ -44,15 +36,11 @@ export default function Services() {
             </div>
             <div className="grid grid-rows-3 gap-4">
               <div className="bg-[#F6FF95] p-8 min-h-[150px] rounded-lg transition-transform duration-200 hover:scale-105">
-                {" "}
-                {/* Added transition and hover scale */}
                 <p className="text-gray-900 text-lg sm:text-3xl">
                   Decentralized dispute resolver
                 </p>
               </div>
               <div className="bg-[#2F2F32] row-span-2 p-8 min-h-[150px] rounded-lg transition-transform duration-200 hover:scale-105">
-                {" "}
-                {/* Added transition and hover scale */}
                 <p className="text-white text-lg sm:text-3xl">
                   Solve dispute cases between seller and buyer effectively
                 </p>
